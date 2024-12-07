163ac1a2b1d74d2895045730f565cf1639aa9233
Abdalrhman119 <wboody68@gmail.com>
2024-12-07 03:42:42 +0200

    Solve Assigment01C#

--- Assigment01C#/Program.cs
+++ Assigment01C#/Program.cs
@@ -70,7 +70,7 @@ namespace Assigment01C_
             #endregion
             #region Q10 : What will be the output of the C# code given below?
             //int num = 1, z = 5;
-
+            //
             //if (!(num <= 0))
             //    Console.WriteLine(++num + z++ + " " + ++z);
             //else
